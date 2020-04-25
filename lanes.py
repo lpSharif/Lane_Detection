95857f7debd5f6ef5f61c286ebc83310e9180b79
Sharif Ahamed <sharifahamed28.sa@gmail.com>
2020-04-26 02:30:19 +0600

    Canny function Added

--- lanes.py
+++ lanes.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
 
+def canny(image):
+    lane_image=np.copy(image)
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    canny = cv2.Canny(blur,50,150)
+    return canny
+
 image = cv2.imread("test_image.jpg")
-lane_image=np.copy(image)
-gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray,(5,5),0)
-canny = cv2.Canny(blur,50,150)
+
+canny=canny(image)
 cv2.imshow('result',canny)
 cv2.waitKey(0)
